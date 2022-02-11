24ee6ae3404be170775ad28db0acadacb4e79e43
udaikue <ikueikue@gmail.com>
2022-02-12 01:50:30 +0900

    Fix for adaptation for Vue2

--- app/javascript/new_form.js
+++ app/javascript/new_form.js
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import NewForm from './new-form.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const selector = '#js-new-form'
   if (document.querySelector(selector)) {
-    createApp(NewForm).mount(selector)
+    new Vue(NewForm).$mount(selector)
   }
 })

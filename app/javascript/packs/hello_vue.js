37978a519a3d76fff081cf4d4b86e24ed8618b5d
udaikue <ikueikue@gmail.com>
2022-02-16 19:57:48 +0900

    Delete unnecessary files

--- app/javascript/packs/hello_vue.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from '../app.vue'
-
-document.addEventListener('DOMContentLoaded', () => {
-  const selector = '#js-hello-vue'
-  if (document.querySelector(selector)) {
-    createApp(App).mount(selector)
-  }
-})

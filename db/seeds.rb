97dabc82a982bb48455d852eb4893bb8ff686f1e
udaikue <ikueikue@gmail.com>
2022-02-18 13:29:25 +0900

    Fix according to Rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
-Book.create!(title: "現場で使えるRuby on Rails5速習実践ガイド", memo: "めっちゃ役に立つ。", paper: true)
-Book.create!(title: "独習Git", memo: "輪読会: 月火金21:00~22:00", paper: true)
-Book.create!(title: "独習Ruby on Rails", memo: "輪読会: りんどく.rbがない日の8:00~9:00 \n 改めてじっくり読むと初心者向けでいい本。", paper: true)
+# frozen_string_literal: true
+
+Book.create!(title: '現場で使えるRuby on Rails5速習実践ガイド', memo: 'めっちゃ役に立つ。', paper: true)
+Book.create!(title: '独習Git', memo: '輪読会: 月火金21:00~22:00', paper: true)
+Book.create!(title: '独習Ruby on Rails', memo: "輪読会: りんどく.rbがない日の8:00~9:00 \n 改めてじっくり読むと初心者向けでいい本。", paper: true)
